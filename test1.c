91a0e2389784be3f33b33341880e29450caad1df
pi <oliverbs163@gmail.com>
2020-04-25 01:34:57 -0700

    add kaleidoscope tutorial

---
+++ test1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello world\n");
+    int num;
+ 
+    scanf("%i", &num);
+    printf("%i\n", num / 2);
+    return 0;
+}
+
+void hello() {
+    return;
+}
